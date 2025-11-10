3de3b1f0a103716cf1a19f2960b5bf8d25ca87e0
Reiner Huechting <dev@reinerhuechting.de>
2025-11-10 22:37:20 +0100

    Add comments to list basics.

--- lists/basics.go
+++ lists/basics.go
@@ -5,26 +5,45 @@ import (
 	"linkedlists/elements"
 )
 
+// New erstellt eine neue leere Liste.
 func New() *List {
 	return &List{head: elements.NewEmpty()}
 }
 
+// IsEmpty gibt an, ob die Liste leer ist.
 func (l *List) IsEmpty() bool {
 	return l.head.IsEmpty()
 }
 
-func (l *List) Append(value int) {
-	l.head.Append(value)
-}
-
+// String liefert eine menschlich lesbare Darstellung der Liste.
 func (l *List) String() string {
 	return fmt.Sprintf("[%v]", l.head)
 }
 
-func (l *List) Length() int {
-	return l.head.Length()
+// Append fügt ein neues Element mit dem gegebenen Wert am Ende der Liste ein.
+func (l *List) Append(value int) {
+	// HINWEIS:
+	// Delegiere die Arbeit an das Kopf-Element der Liste.
+	// D.d. rufe die Append-Methode des Kopf-Elements auf.
+	// begin:solution
+	l.head.Append(value)
+	// end:solution
 }
 
+// Length gibt die Anzahl der Elemente in der Liste zurück.
+func (l *List) Length() int {
+	// HINWEIS:
+	// Delegiere die Arbeit an das Kopf-Element der Liste.
+	// begin:solution
+	return l.head.Length()
+	// end:solution
+}
+
+// Contains gibt an, ob ein Element mit dem gegebenen Wert in der Liste enthalten ist.
 func (l *List) Contains(value int) bool {
+	// HINWEIS:
+	// Delegiere die Arbeit an das Kopf-Element der Liste.
+	// begin:solution
 	return l.head.Contains(value)
+	// end:solution
 }
